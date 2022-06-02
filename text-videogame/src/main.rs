21acf0a29d37ef567e7de4c51f6f0a6cccb9ff3b
Faustino Correa <faustino.correa.m@gmail.com>
2022-06-02 19:34:03 -0400

    show text

--- text-videogame/src/main.rs
+++ text-videogame/src/main.rs
@@ -7,4 +7,12 @@ fn main() {
 
     let content = fs::read_to_string(FILENAME).unwrap();
     println!("{}", content);
+
+    let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
+
+    for result in rdr.records() {
+
+        println!("texto: {}", result.unwrap().get(2).unwrap().trim());
+
+    }
 }
