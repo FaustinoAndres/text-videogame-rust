6cf8f333cbf8a6cc96505a784cc0444eb5073ced
Faustino Correa <faustino.correa.m@gmail.com>
2022-06-02 20:05:04 -0400

    hashmaps

--- text-videogame/src/main.rs
+++ text-videogame/src/main.rs
@@ -1,9 +1,38 @@
 use csv::{ReaderBuilder, StringRecord};
+use std::collections::{HashMap};
 use std::{fs};
 
 const FILENAME: &str = "history.csv";
 
+// TIPO, TAG, TEXTO, VIDA
+
+#[derive(Debug)]
+struct DatoHistoria {
+    tipo_dato: String,
+    tag: String,
+    texto: String,
+    vida: i32
+}
+
+impl DatoHistoria {
+    fn new(row: StringRecord) -> DatoHistoria {
+
+        let vida = row.get(3).unwrap().trim();
+        let vida : i32 = vida.parse().unwrap_or(0);
+
+        return DatoHistoria {
+            tipo_dato: row.get(0).unwrap().trim().to_string(),
+            tag: row.get(1).unwrap().trim().to_string(),
+            texto: row.get(2).unwrap().trim().to_string(),
+            vida: vida,
+        };
+
+    }
+}
+
+
 fn main() {
+    let mut datos_historia: HashMap<String, DatoHistoria> = HashMap::new();
 
     let content = fs::read_to_string(FILENAME).unwrap();
     println!("{}", content);
@@ -11,8 +40,11 @@ fn main() {
     let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
 
     for result in rdr.records() {
-
-        println!("texto: {}", result.unwrap().get(2).unwrap().trim());
-
+        let result = result.unwrap();
+        let dato = DatoHistoria::new(result);
+        datos_historia.insert(dato.tag.clone(), dato);
     }
+
+    println!("{:?}", datos_historia["DERECHA"]);
+
 }
